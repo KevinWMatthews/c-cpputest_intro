6d057f9ddb1dd5d6f6025ae322974d9a81479e30
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-12-08 14:25:52 -0800

    Add skeleton of module for memory leak checks

---
+++ cpputest_intro/tests/test_memory.cpp
@@ -0,0 +1,22 @@
+extern "C"
+{
+#include "memory.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(memory)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(memory, wiring_check)
+{
+    FAIL("wiring check");
+}
